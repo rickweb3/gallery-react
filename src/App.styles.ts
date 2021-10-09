f7216f555ffc30201b42fd20f27e072362a56276
Henrique Prado <henrique.mxnero@gmail.com>
2021-10-09 12:25:53 -0300

    Envio de fotos da máquina do cliente para o firebase, após o envio a mesma já é renderizada na página inicial do aplicativo

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -8,7 +8,7 @@ export const Container = styled.div`
 
 export const Area = styled.div`
     margin: auto;
-    max-widht: 980px;
+    max-width: 980px;
     padding: 30px 0;
 `;
 
@@ -32,4 +32,26 @@ export const PhotoList = styled.div`
     display: grid;
     grid-template-columns: repeat(4, 1fr);
     gap: 10px;
+`;
+
+export const UploadForm = styled.form`
+    background-color: #3D3F43;
+    padding: 15px;
+    border-radius: 10px;
+    margin-bottom: 30px;
+
+    input[type=submit] {
+        background-color: #756DF4;
+        border: 0;
+        color: #FFF;
+        padding: 8px 16px;
+        font-size: 15px;
+        border-radius: 10px;
+        margin: 0 20px;
+        cursor: pointer;
+
+        &:hover {
+            opacity: .9;
+        }
+    }
 `;
--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, FormEvent } from 'react';
 import * as C from './App.styles';
 import * as Photos from './services/photos';
 import { Photo } from './types/Photo';
@@ -6,6 +6,7 @@ import { PhotoItem } from './components/PhotoItem';
 
 const App = () => {
 
+  const [uploading, setUploading] = useState(false);
   const [loading, setLoading] = useState(false);
   const [photos, setPhotos] = useState<Photo[]>([]);
 
@@ -18,12 +19,38 @@ const App = () => {
     getPhotos();
   }, []);
 
+
+  const handleFormSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    const file = formData.get('image') as File;
+
+    if(file && file.size > 0) {
+      setUploading(true);
+      let result = await Photos.insert(file);
+      setUploading(false);
+
+      if(result instanceof Error) {
+        alert(`${result.name} - ${result.message}`);
+      } else {
+        let newPhotoList = [...photos];
+        newPhotoList.push(result);
+        setPhotos(newPhotoList);
+      }
+    }
+  }
+
   return (
     <C.Container>
       <C.Area>
         <C.Header>Galeria de Fotos</C.Header>
         
-        {/** Área de upload */}
+        <C.UploadForm method="POST" onSubmit={handleFormSubmit}>
+          <input type="file" name="image"/>
+          <input type="submit" value="Enviar"/>
+          {uploading && "Enviando..."}
+        </C.UploadForm>
 
         {loading &&
           <C.ScreenWarning>
